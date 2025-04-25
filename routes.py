e5078e3444708d87b70902f3f4e9db4613033226
Aurelle Foko Kengne <aurellefoko117@gmail.com>
2025-04-25 02:07:11 +0200

    modification dashboard.js

--- routes.py
+++ routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User, mysql
-import mysql.connector
+#import mysql.connector
 
 # Création du blueprint pour les routes d'authentification
 auth = Blueprint('auth', __name__)
